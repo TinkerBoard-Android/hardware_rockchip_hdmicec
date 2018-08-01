03da0121b89f0dcb86d405d3950a485983ce4b1a
shin_lin <Shin_Lin@asus.com>
2018-08-01 16:40:44 +0800

    HDMI CEC : Patch in hardware/hdmicec
    Change-Id: Idb9c8cf0cc68ffc17ec9a7dee3808b92d3e9ba71

--- hdmicec_event.cpp
+++ hdmicec_event.cpp
@@ -38,95 +38,182 @@ static int validcecmessage(hdmi_event_t cec_event)
 	int ret = 0;
 
 	if (cec_event.cec.length > 15)
-		ret = 1;
+		ret = -E2BIG;
 	return ret;
+}
+
+/*
+ * Kernel can't report initial HPD state, because when HDMI
+ * driver is initialized HAL has not yet created cec driver's
+ * adapter devnode. HDMI HPD state can't be reported without
+ * adapter devnode, so we should get HDMI initial HPD state
+ * through HDMI HPD state node when cec adapter just finished
+ * initialization.
+ */
+static bool get_hpd_state_from_node(hdmi_cec_context_t* ctx)
+{
+	int ret, fd;
+	char statebuf[20];
+
+	ALOGI("%s", __func__);
+	memset(statebuf, 0, sizeof(statebuf));
+	fd = open(HDMI_STATE_PATH, O_RDONLY);
+	if (fd < 0)
+		return -ENOENT;
+	ret = read(fd, statebuf, sizeof(statebuf));
+	close(fd);
+	if (ret < 0) {
+		ALOGE("read hdmi state err\n");
+		return -EINVAL;
+	}
+
+	if (!strcmp(statebuf, "connected\n")) {
+		return HDMI_CONNECTED;
+	} else if (!strcmp(statebuf, "disconnected\n")) {
+		return HDMI_NOT_CONNECTED;
+	}
+
+	ALOGE("%s can't get hdmi status HDMI_NOT_CONNECTED\n", __func__);
+	return HDMI_NOT_CONNECTED;
+}
+
+static void report_hdp_event(hdmi_cec_context_t* ctx, bool hpd)
+{
+	hdmi_event_t cec_event;
+
+	cec_event.type = HDMI_EVENT_HOT_PLUG;
+	cec_event.dev = &ctx->device;
+	if (hpd)
+		cec_event.hotplug.connected = HDMI_CONNECTED;
+	else
+		cec_event.hotplug.connected = HDMI_NOT_CONNECTED;
+	cec_event.hotplug.port_id = HDMI_CEC_PORT_ID;
+	if (ctx->event_callback)
+		ctx->event_callback(&cec_event, ctx->cec_arg);
 }
 
 static void *uevent_loop(void *param)
 {
-	const int MAX_DATA = 64;
-	static char vdata[MAX_DATA];
 	hdmi_cec_context_t * ctx = reinterpret_cast<hdmi_cec_context_t *>(param);
 	char thread_name[64] = HDMI_CEC_UEVENT_THREAD_NAME;
 	hdmi_event_t cec_event;
 	struct pollfd pfd[2];
 	int fd[2];
+	int ret, i;
 
 	prctl(PR_SET_NAME, (unsigned long) &thread_name, 0, 0, 0);
 	setpriority(PRIO_PROCESS, 0, HAL_PRIORITY_URGENT_DISPLAY);
 
-	fd[0] = open("/sys/devices/virtual/misc/cec/stat", O_RDONLY);
+	fd[0] = ctx->fd;
 	if (fd[0] < 0) {
 		ALOGE ("%s:not able to open cec state node", __func__);
 		return NULL;
 	}
 
-	// Read once from the fds to clear the first notify
-	pread(fd[0], vdata , MAX_DATA, 0);
 	pfd[0].fd = fd[0];
-        if (pfd[0].fd >= 0)
-        	pfd[0].events = POLLPRI | POLLERR;
-	do {
-		int err = poll(pfd, 1, -1);
-		if(err > 0) {
-			if (pfd[0].revents & POLLPRI) {
-				int len = pread(pfd[0].fd, vdata, MAX_DATA, 0);
-				if (len < 0) {
-					// If the read was just interrupted - it is not a
-					// fatal error. Just continue in this case
-					ALOGE ("%s: Unable to read cec state %s",
-					    __FUNCTION__, strerror(errno));
-					continue;
-				}
-				const char *str = vdata;
-				int state = strtoull(str, NULL, 0);
-				ALOGD("cec state is %d", state);
-				memset(&cec_event, 0, sizeof(hdmi_event_t));
-				if (state == 0 || state == 1) {
-					ALOGD("%s sending hotplug: connected = %d ",
-				 	      __FUNCTION__, state);
-					cec_event.type = HDMI_EVENT_HOT_PLUG;
-					cec_event.dev = &ctx->device;
-					cec_event.hotplug.connected = state;
-					cec_event.hotplug.port_id = HDMI_CEC_PORT_ID;
-					if (ctx->event_callback)
-						ctx->event_callback(&cec_event, ctx->cec_arg);
-				} else if (state == 2) {
-					if (ctx->fd < 0) {
-						ALOGE("%s hdmi cec open error", __FUNCTION__);
-						continue;
-					}
-					struct cec_framedata cecframe;	
-					int ret = ioctl(ctx->fd, HDMI_IOCTL_CECREAD, &cecframe);
-					if (ret < 0) {
-						ALOGE("%s hdmi cec read error", __FUNCTION__);
-						continue;
-					}
+	if (pfd[0].fd >= 0)
+		pfd[0].events = POLLIN | POLLRDNORM | POLLPRI;
+
+	while (true) {
+		int err = poll(&pfd[0], 1, 20);
+
+		if (!err) {
+			continue;
+		} else if(err > 0) {
+			if (!ctx->enable || !ctx->system_control)
+				continue;
+			ALOGD("poll revent:%02x\n", pfd[0].revents);
+			memset(&cec_event, 0, sizeof(hdmi_event_t));
+			if (pfd[0].revents & (POLLIN)) {
+				struct cec_msg cecframe;
+
+				ALOGD("poll receive msg\n");
+				ret = ioctl(pfd[0].fd, CEC_RECEIVE, &cecframe);
+				if (!ret) {
 					cec_event.type = HDMI_EVENT_CEC_MESSAGE;
 					cec_event.dev = &ctx->device;
-					cec_event.cec.initiator = (cec_logical_address_t)(cecframe.srcdestaddr >> 4);
-					cec_event.cec.destination = (cec_logical_address_t)(cecframe.srcdestaddr & 0x0f);
-					cec_event.cec.length = cecframe.argcount + 1;
-					cec_event.cec.body[0] = cecframe.opcode;
-					if (validcecmessage(cec_event)) {
+					cec_event.cec.initiator = (cec_logical_address_t)(cecframe.msg[0] >> 4);
+					cec_event.cec.destination = (cec_logical_address_t)(cecframe.msg[0] & 0x0f);
+					cec_event.cec.length = cecframe.len - 1;
+					cec_event.cec.body[0] = cecframe.msg[1];
+					if (!validcecmessage(cec_event)) {
+						for (ret = 0; ret < cec_event.cec.length; ret++)
+						     cec_event.cec.body [ret + 1] = cecframe.msg[ret + 2];
+						for (i = 0; i < cecframe.len; i++)
+							ALOGD("poll receive msg[%d]:%02x\n", i, cecframe.msg[i]);
+						if (ctx->event_callback)
+							ctx->event_callback(&cec_event, ctx->cec_arg);
+					} else {
 						ALOGE("%s cec_event length > 15 ", __func__);
-						continue;
 					}
-					for (ret = 0; ret < cecframe.argcount; ret++) {
-						cec_event.cec.body [ret + 1] = cecframe.args[ret];
-					}
-					if (ctx->event_callback)
-						ctx->event_callback(&cec_event, ctx->cec_arg);
+				} else {
+					ALOGE("%s hdmi cec read error", __FUNCTION__);
 				}
 			}
-		
+
+			if (pfd[0].revents & (POLLPRI)) {
+				int state = -1;
+				struct cec_event event;
+
+				ALOGI("poll receive event\n");
+				ret = ioctl(pfd[0].fd, CEC_DQEVENT, &event);
+				if (!ret) {
+					ALOGD("event:%d\n", event.event);
+					if (event.event == CEC_EVENT_PIN_HPD_LOW) {
+						ALOGI("CEC_EVENT_PIN_HPD_LOW\n");
+						ctx->hotplug = false;
+						cec_event.type = HDMI_EVENT_HOT_PLUG;
+						cec_event.dev = &ctx->device;
+						cec_event.hotplug.connected = HDMI_NOT_CONNECTED;
+						cec_event.hotplug.port_id = HDMI_CEC_PORT_ID;
+						if (ctx->event_callback)
+							ctx->event_callback(&cec_event, ctx->cec_arg);
+					} else if (event.event == CEC_EVENT_PIN_HPD_HIGH) {
+						ALOGI("CEC_EVENT_PIN_HPD_HIGH\n");
+						ctx->hotplug = true;
+						cec_event.type = HDMI_EVENT_HOT_PLUG;
+						cec_event.dev = &ctx->device;
+						cec_event.hotplug.connected = HDMI_CONNECTED;
+						cec_event.hotplug.port_id = HDMI_CEC_PORT_ID;
+						if (ctx->event_callback)
+							ctx->event_callback(&cec_event, ctx->cec_arg);
+					} else if (event.event == CEC_EVENT_STATE_CHANGE) {
+						ALOGD("adapt state change,phy_addr:%x,flags:%x\n", event.state_change.phys_addr, event.flags);
+
+						/*
+						 * Before cec HAL is initialized, hdmi hpd state may be
+						 * changed. So we should confirm the hpd status
+						 * after cec is initialized(Kernel will report
+						 * CEC_EVENT_FL_INITIAL_STATE to notify HAL that
+						 * initialization is done).
+						 */
+						if (event.flags & CEC_EVENT_FL_INITIAL_STATE) {
+							ALOGD("cec adapter init complete, get connect state\n");
+							ctx->hotplug = get_hpd_state_from_node(ctx);
+
+							/*
+							 * Framework will start la polling when box turn on,
+							 * In addition, as soon as framewrok receives hdmi
+							 * plug in, it will start la polling immediately.
+							 * There is not need to report plug in event if hdmi
+							 * is connecting when box turn on. So we should report
+							 * hdmi plug out only.
+							 */
+							if (!ctx->hotplug)
+								report_hdp_event(ctx, ctx->hotplug);
+						}
+						ctx->phy_addr = event.state_change.phys_addr;
+					}
+				} else {
+					ALOGE("%s cec event get err, ret:%d\n", __func__, ret);
+				}
+			}
 		} else {
 			ALOGE("%s: cec poll failed errno: %s", __FUNCTION__,
-		              strerror(errno));
+			strerror(errno));
 			continue;
 		}
-        } while (true);
-
+	}
 	return NULL;
 }
 
@@ -134,12 +221,12 @@ void init_uevent_thread(hdmi_cec_context_t* ctx)
 {
 	pthread_t uevent_thread;
 	int ret;
-	
+
 	ALOGI("Initializing UEVENT Thread");
 	ret = pthread_create(&uevent_thread, NULL, uevent_loop, (void*) ctx);
 	if (ret) {
 		ALOGE("%s: failed to create %s: %s", __FUNCTION__,
-			HDMI_CEC_UEVENT_THREAD_NAME, strerror(ret));
+		      HDMI_CEC_UEVENT_THREAD_NAME, strerror(ret));
 	}
 }
 
